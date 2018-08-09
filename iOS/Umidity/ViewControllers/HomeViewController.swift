c90e6e2e2d2baec9b8056d16738df684a8f8413c
Thomas Münzl <thomasmuenzl@icloud.com>
2018-08-09 15:25:33 +0200

    initial commit

---
+++ iOS/Umidity/ViewControllers/HomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewController.swift
+//  Umidity
+//
+//  Created by Thomas Münzl on 09.08.18.
+//  Copyright © 2018 Thomas Münzl. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+	
+	}
+    
+
+	
+
+}
