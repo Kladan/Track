310198d663cb071bd7f9c7c225ae96828efd7145
Denis Diewold <d.diewold@gmx.net>
2018-08-09 21:48:13 +0200

    Cleanup.

--- HardWare/debug/Libraries/EMlib/3rd-party/EMLib/usb/src/em_usbdint.d
+++
@@ -1,2 +0,0 @@
-C:/Users/Joghurtdrop/XDK-Workspace/XdkApplicationTemplate/debug/objects/./../Libraries/EMlib/3rd-party/EMLib/usb/src/em_usbdint.o: \
- ../Libraries/EMlib/3rd-party/EMLib/usb/src/em_usbdint.c
--- HardWare/debug/objects/source/cJSON.d
+++
@@ -1,4 +0,0 @@
-C:/Users/Joghurtdrop/XDK-Workspace/XdkApplicationTemplate/debug/objects/source/cJSON.o: \
- source/cJSON.c ../Common/include/cJSON.h
-
-../Common/include/cJSON.h:
