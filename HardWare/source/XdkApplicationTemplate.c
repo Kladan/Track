329483904c17b1c55b12512ff3b068b11caa9927
Denis Diewold <d.diewold@gmx.net>
2018-08-09 22:13:54 +0200

    CheckStyle.

--- HardWare/source/XdkApplicationTemplate.c
+++ HardWare/source/XdkApplicationTemplate.c
@@ -83,6 +83,7 @@
 #define TIMERBLOCKTIME UINT32_C(0xffff)
 #define TIMER_AUTORELOAD_ON pdTRUE
 #define TIMER_AUTORELOAD_OFF pdFALSE
+#define HOURS(x) ((portTickType) (x * 3600000) / portTICK_RATE_MS)
 #define SECONDS(x) ((portTickType) (x * 1000) / portTICK_RATE_MS)
 #define MILLISECONDS(x) ((portTickType) x / portTICK_RATE_MS)
 
@@ -100,6 +101,7 @@ char * outputMqtt;
 
 int32_t getTemperature = INT32_C(0);
 uint32_t getHumidity = INT32_C(0);
+uint32_t getPressure = INT32_C(0);
 
 /* global variables ********************************************************* */
 
@@ -113,7 +115,7 @@ uint32_t getHumidity = INT32_C(0);
  * @brief This is a template function where the user can write his custom application.
  *
  */
-void initDeviceId(void){
+void initDeviceId(void) {
 	xdkID = (char*) malloc(16);
     unsigned int * serialStackAddress0 = 0xFE081F0;
     unsigned int * serialStackAddress1 = 0xFE081F4;
@@ -125,7 +127,7 @@ void initDeviceId(void){
     outputMqtt = (char*) malloc(50*sizeof(char));
 }
 
-void initGPIO(void){
+void initGPIO(void) {
 	GPIO_PinModeSet(gpioPortD, 5, gpioModeInputPull, 0);
 	GPIO_PinOutClear(gpioPortD, 5);
 
@@ -136,7 +138,7 @@ void initGPIO(void){
 	ADC_InitSingle(ADC0, &channelInit);
 }
 
-uint32_t getSolidData(void){
+uint32_t getSoilData(void) {
 	uint32_t AdcSample = 0;
 	while ((ADC0->STATUS & (ADC_STATUS_SINGLEACT)) && (BSP_UNLOCKED == ADCLock));
 	__disable_irq();
@@ -152,51 +154,51 @@ uint32_t getSolidData(void){
 	return AdcSample;
 }
 
-static void publish(void){
-//  static char *pub_message = "Hello David";
-  static char *pub_topic = "SensorData";
-  static StringDescr_T pub_topic_descr;
-  StringDescr_wrap(&pub_topic_descr, pub_topic);
+static void publish(void) {
+	static char *pub_topic = "SensorData";
+	static StringDescr_T pub_topic_descr;
+	StringDescr_wrap(&pub_topic_descr, pub_topic);
 
-  Mqtt_publish(session_ptr, pub_topic_descr, outputMqtt,
+	Mqtt_publish(session_ptr, pub_topic_descr, outputMqtt,
                 strlen(outputMqtt), MQTT_QOS_AT_MOST_ONE, false);
 }
 
-void printData(xTimerHandle xTimer){
+void printData(xTimerHandle xTimer) {
 	(void) xTimer;
 
 	Environmental_readHumidity(xdkEnvironmental_BME280_Handle,&getHumidity);
 	Environmental_readTemperature(xdkEnvironmental_BME280_Handle,&getTemperature);
+	Environmental_readPressure(xdkEnvironmental_BME280_Handle,&getPressure);
 
 	sprintf(outputMqtt, "{\
 			\"deviceIdentifier\": %s,\
 			\"soilHumidity\": %lu,\
 			\"temperature\": %ld,\
-			\"environmentHumidity\": %lu}", \
-			xdkID,(unsigned long)getSolidData(), (long) (getTemperature-6)/1000, (unsigned long) getHumidity );
+			\"environmentHumidity\": %lu,\
+			\"pressure\": %lu}", \
+			xdkID,(unsigned long)getSoilData(), (long) (getTemperature-6)/1000, (unsigned long) getHumidity, (unsigned long) getPressure);
 	publish();
 	// printf("Messweert: Test\n\r");
 }
 
 
 
-void createTimer(TimerCallbackFunction_t callback)
-{
-  timerHandle = xTimerCreate(
-    (const char * const) "My Timer", // used only for debugging purposes
-    SECONDS(3),                     // timer period
-    TIMER_AUTORELOAD_ON,             // Autoreload on or off - should the timer
+void createTimer(void) {
+	timerHandle = xTimerCreate(
+		(const char * const) "My Timer", // used only for debugging purposes
+		SECONDS(3),                     // timer period
+		TIMER_AUTORELOAD_ON,             // Autoreload on or off - should the timer
                                      // start again after it expired?
-    NULL,                            // optional identifier
-	callback                  // static callback function
-  );
-  if(NULL == timerHandle) {
-    assert(pdFAIL);
-    return;
-  }
+		NULL,                            // optional identifier
+		printData                  // static callback function
+	);
+	if(NULL == timerHandle) {
+		assert(pdFAIL);
+		return;
+	}
 }
 
-void printOwnIp(void){
+void printOwnIp(void) {
 	NetworkConfig_IpSettings_T myIp;
 	NetworkConfig_GetIpSettings(&myIp);
 
@@ -208,7 +210,7 @@ void printOwnIp(void){
 	(unsigned int) (NetworkConfig_Ipv4Byte(myIp.ipV4, 0)));
 }
 
-void connectToWifi(void){
+void connectToWifi(void) {
 	Retcode_T retStatusConnect = (Retcode_T) WlanConnect_WPA(connectSSID,
 	            connectPassPhrase,0);
 	if (retStatusConnect == RETCODE_OK) {
@@ -216,7 +218,7 @@ void connectToWifi(void){
 	}
 }
 
-void initWifi(void){
+void initWifi(void) {
 	WlanConnect_Init();
 	NetworkConfig_SetIpDhcp(0);
 	connectToWifi();
@@ -225,109 +227,108 @@ void initWifi(void){
 	PAL_socketMonitorInit();
 }
 
-static void handle_connection(MqttConnectionEstablishedEvent_T connectionData){
-  int rc_connect = (int) connectionData.connectReturnCode;
-  printf("Connection Event:\n\r"
+static void handle_connection(MqttConnectionEstablishedEvent_T connectionData) {
+	int rc_connect = (int) connectionData.connectReturnCode;
+	printf("Connection Event:\n\r"
           "\tServer Return Code: %d (0 for success)\n\r",
           (int) rc_connect);
 }
 
 retcode_t event_handler(MqttSession_T* session, MqttEvent_t event,
               const MqttEventData_t* eventData) {
-  BCDS_UNUSED(session);
-  switch(event){
-    case MQTT_CONNECTION_ESTABLISHED:
-      handle_connection(eventData->connect);
-      // subscribing and publishing can now be done
-      BaseType_t timerResult = xTimerStart(timerHandle, TIMERBLOCKTIME);
-      if(pdTRUE != timerResult) {
-        assert(pdFAIL);
-      }
-      // publish();
-      break;
-    case MQTT_CONNECTION_ERROR:
-      handle_connection(eventData->connect);
-      break;
-    case MQTT_INCOMING_PUBLISH:
-      break;
-    case MQTT_SUBSCRIPTION_ACKNOWLEDGED:
-      printf("Subscription Successful\n\r");
-      break;
-    case MQTT_PUBLISHED_DATA:
-      printf("Publish Successful\n\r");
-      break;
-    default:
-      printf("Unhandled MQTT Event: %d\n\r", event);
-      break;
-  }
-  return RC_OK;
+	BCDS_UNUSED(session);
+	switch(event){
+    	case MQTT_CONNECTION_ESTABLISHED:
+    		handle_connection(eventData->connect);
+    		// subscribing and publishing can now be done
+    		BaseType_t timerResult = xTimerStart(timerHandle, TIMERBLOCKTIME);
+    		if(pdTRUE != timerResult) {
+    			assert(pdFAIL);
+    		}
+    		// publish();
+    		break;
+    	case MQTT_CONNECTION_ERROR:
+    		handle_connection(eventData->connect);
+    		break;
+    	case MQTT_INCOMING_PUBLISH:
+    		break;
+    	case MQTT_SUBSCRIPTION_ACKNOWLEDGED:
+    		printf("Subscription Successful\n\r");
+			break;
+    	case MQTT_PUBLISHED_DATA:
+    		printf("Publish Successful\n\r");
+    		break;
+    	default:
+    		printf("Unhandled MQTT Event: %d\n\r", event);
+    		break;
+	}
+	return RC_OK;
 }
 
-retcode_t initMQTT(void){
-  retcode_t rc_initialize = Mqtt_initialize();
-  if (rc_initialize == RC_OK) {
-    session_ptr = &session;
-    Mqtt_initializeInternalSession(session_ptr);
-  }
-  return rc_initialize;
+retcode_t initMQTT(void) {
+	retcode_t rc_initialize = Mqtt_initialize();
+	if (rc_initialize == RC_OK) {
+		session_ptr = &session;
+		Mqtt_initializeInternalSession(session_ptr);
+	}
+	return rc_initialize;
 }
 
 
-void config_set_target(void){
-  static char mqtt_broker[64];
-  const char *mqtt_broker_format = "mqtt://%s:%d";
-  char server_ip_buffer[13];
-  Ip_Address_T ip;
+void config_set_target(void) {
+	static char mqtt_broker[64];
+	const char *mqtt_broker_format = "mqtt://%s:%d";
+	char server_ip_buffer[13];
+	Ip_Address_T ip;
 
-  PAL_getIpaddress((uint8_t *) MQTT_BROKER_HOST, &ip);
-  Ip_convertAddrToString(&ip, server_ip_buffer);
-  sprintf(mqtt_broker, mqtt_broker_format,
+	PAL_getIpaddress((uint8_t *) MQTT_BROKER_HOST, &ip);
+	Ip_convertAddrToString(&ip, server_ip_buffer);
+	sprintf(mqtt_broker, mqtt_broker_format,
 		  server_ip_buffer, MQTT_BROKER_PORT);
 
-  printf(mqtt_broker);
-  SupportedUrl_fromString(mqtt_broker,
+	printf(mqtt_broker);
+	SupportedUrl_fromString(mqtt_broker,
               (uint16_t) strlen(mqtt_broker), &session_ptr->target);
 }
 
-void config_set_connect_data(void){
-  char *device_name = xdkID;
-  static char *username = "xdk";
-  static char *password = "xdk";
+void config_set_connect_data(void) {
+	char *device_name = xdkID;
+	static char *username = "xdk";
+	static char *password = "xdk";
 
-  session_ptr->MQTTVersion = 3;
-  session_ptr->keepAliveInterval = 100;
-  session_ptr->cleanSession = true;
-  session_ptr->will.haveWill = false;
+	session_ptr->MQTTVersion = 3;
+	session_ptr->keepAliveInterval = 100;
+	session_ptr->cleanSession = true;
+	session_ptr->will.haveWill = false;
 
 
-  StringDescr_T device_name_descr;
-  StringDescr_wrap(&device_name_descr, device_name);
-  session_ptr->clientID = device_name_descr;
+	StringDescr_T device_name_descr;
+	StringDescr_wrap(&device_name_descr, device_name);
+	session_ptr->clientID = device_name_descr;
 
-  StringDescr_T username_descr;
-  StringDescr_wrap(&username_descr, username);
-  session_ptr->username = username_descr;
+	StringDescr_T username_descr;
+	StringDescr_wrap(&username_descr, username);
+	session_ptr->username = username_descr;
 
-  StringDescr_T password_descr;
-  StringDescr_wrap(&password_descr, password);
-  session_ptr->password = password_descr;
+	StringDescr_T password_descr;
+	StringDescr_wrap(&password_descr, password);
+	session_ptr->password = password_descr;
 }
 
-void config_set_event_handler(void){
-  session_ptr->onMqttEvent = event_handler;
+void config_set_event_handler(void) {
+	session_ptr->onMqttEvent = event_handler;
 }
 
-retcode_t connectMQTT(void){
-  retcode_t rc = RC_INVALID_STATUS;
-  rc = Mqtt_connect(session_ptr);
-  if (rc != RC_OK) {
-    printf("Could not connect, error 0x%04x\n", rc);
-  }
-  return rc;
+retcode_t connectMQTT(void) {
+	retcode_t rc = RC_INVALID_STATUS;
+	rc = Mqtt_connect(session_ptr);
+	if (rc != RC_OK) {
+		printf("Could not connect, error 0x%04x\n", rc);
+	}
+	return rc;
 }
 
-void appInitSystem(void * CmdProcessorHandle, uint32_t param2)
-{
+void appInitSystem(void * CmdProcessorHandle, uint32_t param2) {
 
     if (CmdProcessorHandle == NULL)
     {
@@ -340,18 +341,18 @@ void appInitSystem(void * CmdProcessorHandle, uint32_t param2)
     initDeviceId();
     initGPIO();
     Environmental_init(xdkEnvironmental_BME280_Handle);
-    createTimer(printData);
+    createTimer();
     initWifi();
 
     retcode_t rc = RC_MQTT_NOT_CONNECTED;
     rc = initMQTT();
     if(rc == RC_OK){
-      config_set_target();
-      config_set_connect_data();
-      config_set_event_handler();
-      connectMQTT();
+    	config_set_target();
+    	config_set_connect_data();
+    	config_set_event_handler();
+    	connectMQTT();
     } else {
-      printf("Initialize Failed\n\r");
+    	printf("Initialize Failed\n\r");
     }
 }
 /**@} */
